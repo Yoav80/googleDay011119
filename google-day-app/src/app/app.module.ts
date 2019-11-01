2e7cb21d50f2804c7483178b0c0ab65ae3896b54
Yoav80 <ymilkman@gmail.com>
2019-11-01 13:10:06 +0100

    added some stuff for voice

--- google-day-app/src/app/app.module.ts
+++ google-day-app/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- google-day-app/src/app/prod-list/prod-list.component.ts
+++ google-day-app/src/app/prod-list/prod-list.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+}
+
+export interface Language {
+  value: string;
+  viewValue: string;
+}
+
+const {webkitSpeechRecognition} : IWindow = <IWindow>window;
 
 @Component({
   selector: 'app-prod-list',
@@ -7,9 +17,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdListComponent implements OnInit {
 
+  public selectedLang: string;
+  private recognition: any;
+  private final_transcript: string;
+  private languages: Language[] = [
+    {value: 'en', viewValue: 'English'},
+    {value: 'nl', viewValue: 'Dutch'},
+  ];
+  
   constructor() { }
 
   ngOnInit() {
+    if (!('webkitSpeechRecognition' in window)) {
+      console.error('no speech recognition');
+    } else {
+      this.recognition = new webkitSpeechRecognition();
+      this.recognition.continuous = true;
+      this.recognition.interimResults = true;
+    
+      this.recognition.onstart = (e) => {
+        console.log('on start', e);
+      }
+      this.recognition.onresult = (e) => {
+        console.log('onresult', e);
+      }
+      this.recognition.onerror = (e) => {
+        console.log('onerror', e);
+      }
+      this.recognition.onend = (e) => {
+        console.log('onend', e);
+      }
+    }
   }
 
+  startButton(event) {
+    this.final_transcript = '';
+    //this.recognition.lang = select_dialect.value;
+    this.recognition.start();
+  }
 }
